cee61f64d42f51ed7b8b1a36bb4ca1bf682141a3
Cody Cooper <contact@itscodycooper.com>
2016-01-04 22:14:20 -0800

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      $('.parallax').parallax();
+      $('.scrollspy').scrollSpy();
+      $('.materialboxed').materialbox();
+      $('.button-collapse').sideNav({
+          closeOnClick: true
+      });
+
+      ScrollAnimations();
+
+      
+    });
